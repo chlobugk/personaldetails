641338b51cd4e7faf7247fef0b37a8b3cd28a292
Chloe Krainbucher <chloekrainbucher@poiuytrohb8ubh8.home>
2017-04-11 16:26:54 -0400

    added input info

--- app.rb
+++ app.rb
@@ -15,3 +15,17 @@ get '/' do
 	personaldetails=db.exec("SELECT name, age, first_favorite_number, second_favorite_number, third_favorite_number, favorite_food, favorite_phrase, favorite_color, favorite_season FROM personaldetails");
 	erb :index, locals: {personaldetails: personaldetails}
 end
+
+post '/personaldetails' do
+	name = params[:name]
+	age = params[:age]
+	first_favorite_number = params[:first_favorite_number]
+	second_favorite_number = params[:second_favorite_number]
+	third_favorite_number = params[:third_favorite_number]
+	favorite_food = params[:favorite_food]
+	favorite_phrase = params[:favorite_phrase]
+	favorite_color = params[:favorite_color]
+	favorite_season = params[:favorite_season]
+	db.exec("INSERT INTO personaldetails(name, age, first_favorite_number, second_favorite_number, third_favorite_number, favorite_food, favorite_phrase, favorite_color, favorite_season) VALUES('#{name}', '#{age}', '#{first_favorite_number}', '#{second_favorite_number}', '#{third_favorite_number}', '#{favorite_food}', '#{favorite_phrase}', '#{favorite_color}', '#{favorite_season}')");
+	redirect '/'
+end
